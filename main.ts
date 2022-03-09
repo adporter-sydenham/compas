082a2dbb1d12a514834daeae0626034883a55cd8
adporter-sydenham <101246853+adporter-sydenham@users.noreply.github.com>
2022-03-09 09:12:20 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,24 @@
 let CompassBearing = 0
 basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
     CompassBearing = input.compassHeading()
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
     if (CompassBearing > 315 || CompassBearing < 45) {
         basic.showString("N")
+    }
+    if (CompassBearing > 45 || CompassBearing < 135) {
+        basic.showString("E")
+    }
+    if (CompassBearing > 135 || CompassBearing < 225) {
+        basic.showString("S")
+    }
+    if (CompassBearing > 225 || CompassBearing < 315) {
+        basic.showString("S")
     }
     basic.pause(1000)
 })
