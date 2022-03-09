32c9b7adc7722895b6c979faabb42c9d6c9eba27
adporter-sydenham <101246853+adporter-sydenham@users.noreply.github.com>
2022-03-09 09:37:36 +0000

    Nested the ifs

--- main.ts
+++ main.ts
@@ -8,17 +8,23 @@ basic.forever(function () {
         . . . . .
         . . . . .
         `)
+    music.playTone(262, music.beat(BeatFraction.Whole))
     if (CompassBearing > 315 || CompassBearing < 45) {
         basic.showString("N")
-    }
-    if (CompassBearing > 45 || CompassBearing < 135) {
-        basic.showString("E")
-    }
-    if (CompassBearing > 135 || CompassBearing < 225) {
-        basic.showString("S")
-    }
-    if (CompassBearing > 225 || CompassBearing < 315) {
-        basic.showString("S")
+    } else {
+        if (CompassBearing > 45 && CompassBearing < 135) {
+            basic.showString("E")
+        } else {
+            if (CompassBearing > 135 && CompassBearing < 225) {
+                basic.showString("S")
+            } else {
+                if (CompassBearing > 225 && CompassBearing < 315) {
+                    basic.showString("W")
+                } else {
+                    basic.showIcon(IconNames.No)
+                }
+            }
+        }
     }
     basic.pause(1000)
 })
